3cab89f6ac1f41399001ac47852b4ac1b7ea4b95
Zane Stock <zanestock3@gmail.com>
2022-04-25 15:40:09 -0500

    commit tree d427b2b0ee39c51679552b59f44624034561f240

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(books => renderBooks(books))
   
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +19,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+initialize()
